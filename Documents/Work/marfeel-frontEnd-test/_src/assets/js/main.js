20abd09132621b3d89b0333e010b4384cb94ca84
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-10-17 11:43:18 +0200

    reduce functions

--- Documents/Work/marfeel-frontEnd-test/_src/assets/js/main.js
+++ Documents/Work/marfeel-frontEnd-test/_src/assets/js/main.js
@@ -4,7 +4,6 @@ const userCard = document.querySelector('.user--card');
 const userReposContainer = document.querySelector('.user--repos');
 const filter = document.querySelector('.filter--field');
 const searchButton = document.querySelector('.filter--button');
-const API = "https://api.github.com/users/";
 const fullNameElement = document.createElement('h1');
 const logoContainer = document.querySelector('.logo');
 const userContainer = document.querySelector('.user--container');
@@ -27,58 +26,71 @@ function createNewReposElement(myElement, myClass, myContent) {
     return newElement;
 }
 
+function cleanCard() {
+    userCard.innerHTML = '';
+    logoContainer.innerHTML = '';
+    userReposContainer.innerHTML = '';
+}
+
+function createUserElements(fullName, userName) {
+    const img = '<i class="fab fa-github-square"></i>';
+    logoContainer.innerHTML = img;
+    createNewElement('p', 'user--userName', `@${userName}`);
+    fullName ? createNewElement('h1', 'user--fullName', fullName) : createNewElement('h1', 'user--fullName', 'Name not defined');
+    createNewElement('p', 'user--description', 'This is the bio...')
+}
+
+function createReposElements(reposData) {
+    createNewReposElement('ul', 'user--repos__list', '');
+    for (let i = 1; i < reposData.length; i++) {
+        const reposList = document.querySelector('.user--repos__list');
+        reposList.innerHTML +=
+            `<li class="repos__list--item"> 
+                <a class ="repos__list--link" href=${reposData[i].html_url}>Repo ${i}</a>
+                <ul class="list--puntuation">
+                <li class="item--puntuation">
+                        <i class="fas fa-star"></i>
+                        ${reposData[i].stargazers_count}
+                        <i class="fas fa-code-branch"></i>
+                        ${reposData[i].forks_count}
+                </li>
+                </ul>            
+    </li>`;
+    }
+}
 
 function getUserData() {
     const currentUser = filter.value;
+    const API = "https://api.github.com/users/";
     const ENDPOINT = API + currentUser;
-    fetch(ENDPOINT)
-        .then(response => response.json())
+    return (
+        fetch(ENDPOINT)
+            .then(response => response.json()));
+}
+
+function getGitHubData() {
+    getUserData()
         .then(data => {
-            userCard.innerHTML = '';
-            logoContainer.innerHTML = '';
-            userReposContainer.innerHTML = '';
+            cleanCard();
             if (data.message === 'Not Found') {
-                userCard.innerHTML = '';
                 userCard.innerHTML = `<div class="add--container"><p class="add--notFound">Does not exist</p></div>`;
             } else {
-                userCard.innerHTML = '';
                 const fullName = data.name;
                 const userName = data.login;
-                const img = '<i class="fab fa-github-square"></i>';
-                logoContainer.innerHTML = img;
-                createNewElement('p', 'user--userName', `@${userName}`);
-                fullName ? createNewElement('h1', 'user--fullName', fullName) : createNewElement('h1', 'user--fullName', 'Name not defined');
-                createNewElement('p', 'user--description', 'This is the bio...')
-
+                createUserElements(fullName, userName);
                 const repos = data.repos_url;
                 return fetch(repos);
             }
         })
         .then(reposResponse => reposResponse.json())
         .then(reposData => {
-            userReposContainer.innerHTML = '';
             createNewReposElement('h3', 'repos--title', 'Repositories');
             if (reposData.length === 0) {
                 createNewElement('p', 'add--notFound', 'No repositories found')
             } else {
-                createNewReposElement('ul', 'user--repos__list', '');
-                for (let i = 1; i < reposData.length; i++) {
-                    const reposList = document.querySelector('.user--repos__list');
-                    reposList.innerHTML +=
-                        `<li class="repos__list--item"> 
-                            <a class ="repos__list--link" href=${reposData[i].html_url}>Repo ${i}</a>
-                            <ul class="list--puntuation">
-                            <li class="item--puntuation">
-                                    <i class="fas fa-star"></i>
-                                    ${reposData[i].stargazers_count}
-                                    <i class="fas fa-code-branch"></i>
-                                    ${reposData[i].forks_count}
-                            </li>
-                            </ul>            
-                </li>`;
-                }
+                createReposElements(reposData);
             }
         });
 }
 
-searchButton.addEventListener('click', getUserData);
+searchButton.addEventListener('click', getGitHubData);
